da3e57c56cf210b52e441ccdafe3d78894d89927
Fadee Kannah <fadeekannah@gmail.com>
2015-11-18 22:36:38 -0800

    Updated function to add support for converting roman strings to decimal numbers

--- math.roman.js
+++ math.roman.js
@@ -1,10 +1,10 @@
 Math.roman = function(n){
   var roman=['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
-  var value=[1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-  var res;
-  if(!isNaN(parseInt(n,10))){
+  var value=[1,     4,   5,    9,   10,  40,   50,  90,  100,  400,  500, 900, 1000];
+  var res,i;
+  if(!isNaN(parseInt(n,10))){ //if input is a number converts it to roman
     res ='';
-    var i = roman.length-1;
+    i = roman.length-1;
     while(i>=0){
       if(n>=value[i]){
         n -= value[i];
@@ -13,6 +13,33 @@ Math.roman = function(n){
       else{
         i--;
       }
+    }
+  }else{ //else converts it to string
+    var index;
+    res = 0;
+    n = n.toString();
+    //checks if the string is a roman number
+    for(i=0;i<n.length;i++){
+      if(roman.indexOf(n.charAt(i))<0){
+        return 'Not a valid numeral';
+      }
+    }
+    i=0;
+    while(i<n.length){
+      //checks for unique cases
+      if(i<n.length){
+        index = roman.indexOf(n.charAt(i)+ n.charAt(i+1));
+      }
+      //checks index and updates the loop accordingly
+      if(index < 0 ){
+        index = roman.indexOf(n.charAt(i));
+      }
+      else{
+        i++;
+      }
+
+      res += value[index];
+      i++;
     }
   }
   return res;
